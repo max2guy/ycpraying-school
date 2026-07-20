b1fab3bc5bdf76ea5aa5e50cfc279fb6baeb3050
max2guy <max2guy@gmail.com>
2026-07-20 15:11:35 +0900

    미션 사진 최대 장수 안내 추가

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Service Worker Version 124 (v1.5.7)
+// Service Worker Version 125 (v1.5.8)
 
 /* ===== FCM 백그라운드 메시지 — SW 최상단에 초기화 필수 ===== */
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
@@ -46,7 +46,7 @@ self.addEventListener('notificationclick', e => {
 });
 
 /* ===== 캐시 전략 ===== */
-const CACHE_NAME = 'yc-school-v34';
+const CACHE_NAME = 'yc-school-v35';
 
 const FILES_TO_CACHE = [
     './',
